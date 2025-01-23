b01879ddec34cb96fb2ad4f3671773f6c679382a
Anish Raj <anishrajdmk@gmail.com>
2025-01-23 13:22:51 +0530

    Added a foot element to the loading screen and added a nav bar

--- MinorProjectObys/script.js
+++ MinorProjectObys/script.js
@@ -1,34 +1,35 @@
-let timeline1 = gsap.timeline();
-timeline1.from(".line h1", {
-  y: 100,
-  opacity: 0,
-  stagger: 0.2,
-});
-timeline1.from("#line1-part1", {
+function loadingScreenAnimation() {
+  let timeline1 = gsap.timeline();
+  timeline1.from(".line h1", {
+    y: 100,
+    opacity: 0,
+    stagger: 0.2,
+  });
+  timeline1.from("#line1-part1,#footElement h2", {
     duration: 1.5,
     opacity: 0,
-  onStart: () => {
+    onStart: () => {
+      const h5Timer = document.querySelector("#line1-part1 h5");
+      let counter = 0;
+      let t = setInterval(() => {
+        counter++;
+        h5Timer.textContent = counter;
+        if (counter === 100) {
+          clearInterval(t);
+        }
+      }, 30);
+    },
+  });
+  timeline1.to(".line h2", {
+    animationName: "nowAnime",
+    opacity: 1,
+    delay: 0,
+  });
+  timeline1.to("#loader", {
+    opacity: 0,
+    duration: 0.5,
+    delay: 3,
+  });
+}
 
-
-    const h5Timer = document.querySelector("#line1-part1 h5");
-    let counter = 0;
-    let t = setInterval(() => {
-      counter++;
-      h5Timer.textContent = counter;
-      if (counter === 100) {
-        clearInterval(t);
-      }
-    }, 30);
-  },
-});
-timeline1.to(".line h2",{
-    animationName:"nowAnime",
-    opacity:1,
-    delay:0,
-
-});
-timeline1.to("#loader", {
-  opacity: 0,
-  duration: 0.5,
-  delay: 3,
-});
+loadingScreenAnimation();
